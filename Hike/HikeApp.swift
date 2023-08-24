8eeef986b3a78c0778ba17e628e9dbaa1b13093a
Prerit2002 <Prerit715@gmail.com>
2023-08-24 23:54:42 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Prerit Rawtani on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
